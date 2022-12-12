99358dfc973cce24d8a00241cdd7a90fda0d5770
Jonathan Pacheco <jonathan.pacheco@y.uno>
2022-12-12 12:08:24 -0500

    1.1.1

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "YunoSDK",
+    defaultLocalization: "en",
     products: [
         .library(
             name: "YunoSDK",
@@ -12,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YunoSDK",
-            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.1.0/YunoSDK.xcframework.zip",
-            checksum: "93018547d8b784d57d7415f8811ebd9ec1ac96ea5e9cd56be49b0a606fa52cf6"
+            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.1.1/YunoSDK_SPM.xcframework.zip",
+            checksum: "c926bb3960833f329a0c76a921b6852c3078fb68e74b8094871733d8ab0c2ab7"
         )
     ]
 )
