486a45e3e12a97a9ae5525fa0d4b9990b6d195e5
Jonathan Pacheco <jonathan.pacheco@y.uno>
2022-12-12 15:05:26 -0500

    Package checksum

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "YunoSDK",
             url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.1.1/YunoSDK_SPM.xcframework.zip",
-            checksum: "c926bb3960833f329a0c76a921b6852c3078fb68e74b8094871733d8ab0c2ab7"
+            checksum: "4706a47bad3f8f048f885ef632669c1863eeb9fa66572d19f76106926cac27ce"
         )
     ]
 )
