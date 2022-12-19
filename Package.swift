950f7fdbf71ca2ee4312f707bb4d81d0091569d0
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2022-12-19 19:35:11 +0000

    Change version number 1.9.6

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YunoSDK",
-            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.1.3/YunoSDK_SPM.xcframework.zip",
-            checksum: "b381bc00edf5088ed787fbb85a2ef3218177d3b1f3d877a03ba822f2cdf4231a"
+            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.9.6/YunoSDK_SPM.xcframework.zip",
+            checksum: "427ac92a2ab1c2ee18023c2a91dfb071dc47968c9ad683f6dee9d7a9bf0f2670"
         )
     ]
 )
--- YunoSDK.podspec
+++ YunoSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'YunoSDK'
-  s.version          = '1.1.3'
+  s.version          = '1.9.6'
   s.summary          = 'A short description of YunoSDK.'
 
   s.description      = <<-DESC
