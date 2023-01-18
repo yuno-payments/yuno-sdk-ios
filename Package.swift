f82844613f53e17b31608d4fe2aa5c29aacd0e94
Lautaro Pinto <lautaro.pinto@y.uno>
2023-01-18 11:18:39 -0300

    Version chanegd

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YunoSDK",
-            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.1.4/YunoSDK_SPM.xcframework.zip",
+            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.1.5/YunoSDK_SPM.xcframework.zip",
             checksum: "071b6d536dad3f97eb8dd94d4160edfb01b33f198ec30dc18f3dd96de73f1235"
         )
     ]
