814c2ff8acfccd417b335fef6250a46552976251
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2023-03-24 22:19:14 +0000

    Change version number 1.1.15

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YunoSDK",
-            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.1.14/YunoSDK_SPM.xcframework.zip",
-            checksum: "282e335c7f0b81508bdc04748d84bdbc09a064a58ca46f5c28a2e901b42c2c30"
+            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.1.15/YunoSDK_SPM.xcframework.zip",
+            checksum: "4441bb503600d4082858a7cd3645019980836344b379832799ae8c089347139a"
         ),
         .binaryTarget(
             name: "OpenpayKit",
             url: "https://github.com/open-pay/openpay-swift-ios/releases/download/3.2.0/OpenpayKit.xcframework.zip",
-            checksum: "a70869c996b1512e9d5167628d30c4a686dab3c743a8cd85117a194a781b3ae6"
+            checksum: "4441bb503600d4082858a7cd3645019980836344b379832799ae8c089347139a"
         )
     ]
 )
