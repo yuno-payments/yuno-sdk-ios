e5cf93e4e6fd49cc3da6f33dd0bdf205027f18f2
viviamezquita18 <viviamezquita18@gmail.com>
2024-04-24 06:03:56 +1000

    Remove open pay from package.swift and podspect

--- Package.swift
+++ Package.swift
@@ -8,18 +8,13 @@ let package = Package(
     products: [
         .library(
             name: "YunoSDK",
-            targets: ["YunoSDK", "OpenpayKit"]),
+            targets: ["YunoSDK"]),
     ],
     targets: [
         .binaryTarget(
             name: "YunoSDK",
             url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.8.1/YunoSDK_SPM.xcframework.zip",
             checksum: "b1eb8edbdf26ce78e2796de1645e84f27b9ed4c5d157c5fb1cb51572cbc7f66e"
-        ),
-        .binaryTarget(
-            name: "OpenpayKit",
-            url: "https://github.com/open-pay/openpay-swift-ios/releases/download/3.2.0/OpenpayKit.xcframework.zip",
-            checksum: "a70869c996b1512e9d5167628d30c4a686dab3c743a8cd85117a194a781b3ae6"
         )
     ]
 )
