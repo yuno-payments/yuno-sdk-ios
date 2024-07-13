9532f37f23e6f8de47bbacbdc89580c9e42bdc3c
Andres Montoya <andres.montoya@y.uno>
2024-07-13 08:50:46 -0500

    change checksum

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "YunoSDK",
             url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.13.0/YunoSDK_SPM.xcframework.zip",
-            checksum: "f2f881f1a6cacc62cfa7a821bc9301cecf1fd8483e437e2109a2eccac0a88d40"
+            checksum: "74d7a9736dc966f6073a3bd7fc073830f77f4b8e7dcfc66ba6ebe26d9dff83bd"
         )
     ]
 )
