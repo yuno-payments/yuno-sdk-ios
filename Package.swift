cd2a51e42afad26c9a6169d4d1a4eef4699d47a7
Andres Montoya <andres.montoya@y.uno>
2024-09-03 18:37:06 -0500

    add applepay logs

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YunoSDK",
-            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.15.0/YunoSDK_SPM.xcframework.zip",
-            checksum: "cdbac4ab6335a4f59705ee62eec114c6c566a90b8d37a92e0aa8c8672f913f71"
+            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.13.2/YunoSDK_SPM.xcframework.zip",
+            checksum: "c8058f45cbaee310a775490cba4632cad21bd6532af20c8fa6e185fbd7044dab"
         )
     ]
 )
