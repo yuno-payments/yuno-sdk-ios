c2ce7a51c0e41197b8c794c9ffd3af5a8454dae5
Andres Montoya <andres.montoya@y.uno>
2024-11-22 18:43:01 -0500

    change version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YunoSDK",
-            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.19.0/YunoSDK_SPM.xcframework.zip",
+            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.19.1/YunoSDK_SPM.xcframework.zip",
             checksum: "7e87d6f16dd371d9e0e917b81058ec7312cb65933266e4df4b4d52c6e0dfbced"
         )
     ]
