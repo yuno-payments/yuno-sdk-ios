6751975c1380d0801f17c607cab0b32f895fc8d8
viviamezquita18 <viviamezquita18@gmail.com>
2024-12-03 15:41:25 -0500

    Add koin package

--- Package.swift
+++ Package.swift
@@ -9,12 +9,32 @@ let package = Package(
         .library(
             name: "YunoSDK",
             targets: ["YunoSDK"]),
+        .library(
+            name: "YunoKoin",
+            targets: ["YunoKoin", "YunoWrapper"])
     ],
     targets: [
         .binaryTarget(
             name: "YunoSDK",
             url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.20.0/YunoSDK_SPM.xcframework.zip",
             checksum: "001ecc24f527de0d0cca5e447cc96d31264106b4215d99fea3af1ac99ef339ca"
+        ),
+        .binaryTarget(
+            name: "YunoKoin",
+            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.20.0-beta/YunoKoin.xcframework.zip",
+            checksum: "d496080f65dd1bd70cbc4850453cb6f02ffce0d4d172499175a3c2263fc84f8a"
+        ),
+        .binaryTarget(
+            name: "KoinFingerprint",
+            url: "https://github.com/koinlatam/ios-sdk/releases/download/1.2.0/KoinFingerprint-xcframework.zip",
+            checksum: "a0285c8816ed6ea121e1e53142694a1c254e1da34c66890c908af702d6639488"),
+       .target(
+            name: "YunoWrapper",
+            dependencies: [
+                .target(name: "YunoKoin"),
+                .target(name: "KoinFingerprint")
+                ],
+            path: "YunoWrapper"
         )
     ]
 )
