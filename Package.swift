0aa55f5fc39350a27a4345043673a8a064334b75
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2025-05-01 01:07:18 +0000

    Change version number 1.24.1

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YunoSDK",
-            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.24.0/YunoSDK_SPM.xcframework.zip",
-            checksum: "151b3241361756e018a6f09a84da9060896a1798bcced6ea35701e2f05aa7dc8"
+            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/1.24.1/YunoSDK_SPM.xcframework.zip",
+            checksum: "f25d36ce2f44f32a3785393da36650024be9e9e90e1f09069d5b9a24ad059915"
         )
     ]
 )
