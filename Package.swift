dc290d02846711a64975f24c837efb99a185e24a
Viviana Amezquita Mesa <vivi@Vivianas-MacBook-Pro.local>
2026-03-25 11:28:23 -0500

    update checksum

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "YunoSDK",
             url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/2.13.1/YunoSDK_SPM.xcframework.zip",
-            checksum: "17e4bc40d7cacb94f588e920def96a50baf0e3939c550fe856eb486e8017e531"
+            checksum: "296fa7e13f59a77b1ab17805efed916d6be15a2cd356c7ad382ce98432854e45"
         )
     ]
 )
