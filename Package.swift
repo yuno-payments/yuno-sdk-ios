ea5bd4cb8061dcc128f4b71e6fcb314ef49588f8
Viviana Amezquita Mesa <vivi@Vivianas-MacBook-Pro.local>
2026-07-27 19:02:59 -0500

    Change version number 2.21.1

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YunoSDK",
-            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/2.21.0/YunoSDK_SPM.xcframework.zip",
-            checksum: "85e2df7b2f3830479961eaddfa76e55d2d56a46d0b7e99d7dabb9ba1156962d2"
+            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/2.21.1/YunoSDK_SPM.xcframework.zip",
+            checksum: "6ecad782e31954dfd6957c364990fce661fa244b5c5dce121aa8b53ac2df0bf7"
         )
     ]
 )
