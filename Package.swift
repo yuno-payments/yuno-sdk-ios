061e3eca5bd154d4c3c1c6fcc0cc5f2a3c429783
Bitrise CI <noreply@bitrise.io>
2026-08-26 23:50:42 +0000

    Change version number 2.23.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YunoSDK",
-            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/2.22.0/YunoSDK_SPM.xcframework.zip",
-            checksum: "9ee9b6221ee1a63b49d010b288324d703bd969c2220187d051e46d28a47f09bc"
+            url: "https://github.com/yuno-payments/yuno-sdk-ios/releases/download/2.23.0/YunoSDK_SPM.xcframework.zip",
+            checksum: "93dd88955f230a9bc68c25324a6a294546eb9df3e687c1308aeea162f583f6c2"
         )
     ]
 )
